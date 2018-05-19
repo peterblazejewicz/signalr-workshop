e24d8dfb75c8d879d0f293dc8266ae98d35aa774
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-05-19 23:06:49 +0200

    Client app rename

--- AngularChat/AngularChat/Startup.cs
+++ AngularChat/AngularChat/Startup.cs
@@ -24,7 +24,7 @@ namespace AngularChat
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "client/dist";
             });
 
              services.AddSignalR();
@@ -63,7 +63,7 @@ namespace AngularChat
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "client";
 
                 if (env.IsDevelopment())
                 {
